5c8de21b5e98b2410246bcf4d1f570a2b2bbff4b
kahveciderin <kahveciderin@gmail.com>
2020-05-10 23:00:02 +0300

    bişi yapmadım

--- main.c
+++ main.c
@@ -17,13 +17,32 @@ int main(int argc, char *kwargs[]) {
     uint8_t Y;
     uint8_t status;
     ip = ((uint16_t)address_space[0xfffd] << 8) | (uint16_t)address_space[0xfffc];
+    int isInstruction = 0;
+    bool increment = false;
+    uint16_t value = 0x0;
     while (1) {
+        if(isInstruction == 0){
         switch (address_space[ip]) {
             case 0x00:
 
             case 0x01:
 
-            case 0
+            
+            case 0x02:
+            break;
+        }
+        }else if(isInstruction == 1){
+
+        value = address_space[ip]
+        if(increment){
+
+        isInstruction = 2;
+        }else{
+            isInstruction = 0;
+        }
+        } else if(isInstruction == 2){
+            value = ((uint16_t)value << 8) | address_space[ip];
+            isInstruction = 0;
         }
     }
 }
