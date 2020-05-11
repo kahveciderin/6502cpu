a7d1390412536f32a2ac9f8a0c84ea24206cf5e8
bash-the-trash <denizmersinlioglu2005@gmail.com>
2020-05-11 11:40:38 +0300

    modified some stuff

--- get_inst_info.py
+++ get_inst_info.py
@@ -6,6 +6,10 @@ for el in data:
     newdata.append(el)
     if el == '---':
         newdata.append("A")
-
+del data
+data = []
+for el in newdata:
+    el = "AM_" + el.replace(",","_").replace("#", "imm")
+    data.append(el)
 for x in range(256):
-    print(newdata[x * 2 + 1] + ",")
+    print(data[x * 2 + 1] + ",")
