9736c0295e2c8a4904e7bbf94e9a28aeac0753cd
bash-the-trash <denizmersinlioglu2005@gmail.com>
2020-05-11 12:46:36 +0300

    added address touch values

--- get_inst_info.py
+++ get_inst_info.py
@@ -46,7 +46,9 @@ with open("raw_instsize_data.html") as f:
     rawhtml = f.readlines()
 #list(map(lambda x: print(x.split(" ")[0] + ","), rawhtml))
 datasheet_entries = map(lambda x: list(filter(lambda y: len(y) > 0, x.strip().split(" "))), list(filter(lambda x: x.strip().split(" ")[0] in valid_instrs, rawhtml)))
-oplens = [0] * 256
+#datasheet_entries = map(lambda x: , list(filter(lambda x: x.strip().split(" ")[0] in valid_instrs, rawhtml)))
+opjmps = ["UNIMPL"] * 256
 for entry in datasheet_entries:
-    oplens[int(entry[-3], base=16)] = int(entry[-2])
-print(oplens)
+    #oplens[int(entry[-3], base=16)] = int(entry[-2])
+    opjmps[int(entry[-3], base=16)] = entry[1] in jumping_opcodes
+print(opjmps)
